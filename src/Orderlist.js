ca6cb829a8e348978afcc1ce44228b7fa870165b
nurlande <kurmanbekov2222@gmail.com>
2019-09-25 16:36:09 +0600

    Firebase Firestore integration done

--- src/Orderlist.js
+++ src/Orderlist.js
@@ -9,28 +9,35 @@ class Orderlist extends React.Component {
         };
       }
       componentDidMount() {
-          const db = firebase.firestore();
-          const orderlist = db.collection("orders");
-          this.setState({
-              orders: orderlist
-          })
-          console.log(this.state.orders);
+        const db = firebase.firestore();
+        db.collection("orders").get().then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+         // array of cities objects
+        this.setState({
+            orders: data
+        })
+    });
       }
+      check = e => {
+        console.log(this.state.orders);
+      } 
     render() 
     {
+        const listItems = this.state.orders.map((d) => <li key={d.id}>{d.title}</li>);
   return (
     <div>
         <div className="container">
         <h1>Orderlist</h1>
         <div className="jumbotron">
-            <h1 className="display-4">Post title <span>date</span></h1>
+            <h1 className="display-4"> <span>date</span></h1>
             <p className="lead"><b>Details of post</b>: This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-4" />
-            <p>
+            {listItems}
+            <p> 
                 Address from and to
             </p>
 
-            <button className="btn btn-primary btn-lg">End process</button>
+            <button className="btn btn-primary btn-lg" onClick={this.check}>End process</button>
         </div>
         </div>
     </div>
